b60c69a0a8c53b0ac0cdd316259cb2532a07c2c5
Zerone Brianstorm <ometha12@gmail.com>
2024-12-28 22:51:23 +0530

    tests

--- system_monitor.py
+++ system_monitor.py
@@ -31,7 +31,8 @@ def get_battery_percentage():
         # Run the termux-battery-status command
         result = subprocess.run(["termux-battery-status"], capture_output=True, text=True, check=True)
         battery_data = json.loads(result.stdout)
-        return int(battery_data.get("percentage", 0))
+        print(int(battery_data.get("percentage", 0)))
+        return (int(battery_data.get("percentage", 0)))
     except:
         return 0
 
