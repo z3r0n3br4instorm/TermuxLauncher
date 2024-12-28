db23c2124f09794dd52e5e121ef9994ae170e988
Zerone Brianstorm <ometha12@gmail.com>
2024-12-28 23:03:59 +0530

    batt fix

--- battery.py
+++ battery.py
@@ -3,6 +3,6 @@ import json
 try:
     result = subprocess.run(["termux-battery-status"], capture_output=True, text=True, check=True)
     battery_data = json.loads(result.stdout)
-    return battery_data.get("percentage", 0)
+    battery_data.get("percentage", 0)
 except:
     return 0
